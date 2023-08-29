c692ca30a03a3eacc41860a466dca6718cf3ceae
Matej Kolarević <mkolarevic@protonmail.com>
2023-08-29 11:28:02 +0200

    cleanup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Hello world</h1>
   </main>
 </template>
